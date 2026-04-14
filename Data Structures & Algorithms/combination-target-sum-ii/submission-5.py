53f0a063f4312be6202c14f752da2cbfe19d7bb9
Terry Kwok <142947048+terry9041@users.noreply.github.com>
2026-04-13 22:16:50 -0700

    Add: combination-target-sum-ii - submission-5

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-5.py
@@ -0,0 +1,25 @@
+class Solution:
+    def combinationSum2(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        nums.sort()
+    
+        def backtrack(path, total, idx):
+            if total > target:
+                return
+            if total == target:
+                res.append(path[:])
+                return
+            for i in range(idx, len(nums)):
+                # 1. i > idx: This ensures we are not at the very first element of the current loop.
+                # 2. nums[i] == nums[i-1]: This detects a duplicate.
+                # we treat dup previous element at the SAME level of the tree.
+                if i > idx and nums[i] == nums[i-1]:
+                    continue
+
+                path.append(nums[i])
+                backtrack(path, total+nums[i], i+1)
+                path.pop()
+                    
+
+        backtrack([],0,0)
+        return res
