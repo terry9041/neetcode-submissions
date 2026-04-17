0e136214ac18e0890a753983e467f7ec97fd021c
Terry Kwok <142947048+terry9041@users.noreply.github.com>
2026-04-16 19:33:40 -0700

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        # BF: check every jump combination -> max O(2^n)
+        target = len(nums)-1
+        for i in range(len(nums)-2,-1,-1):
+            if i + nums[i] >= target:
+                target = i
+        return target == 0
