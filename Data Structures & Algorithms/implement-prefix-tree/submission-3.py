c437bf0eaeaae9a17cd6c8111d45455c70d503c3
Terry Kwok <142947048+terry9041@users.noreply.github.com>
2026-04-27 19:12:24 -0700

    Add: implement-prefix-tree - submission-3

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-3.py
@@ -0,0 +1,40 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [None] * 26
+        self.endOfWord = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            pos = ord(c) - ord("a")
+            if curr.children[pos] == None:
+                curr.children[pos] = TrieNode()
+            curr = curr.children[pos]
+        curr.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for c in word:
+            pos = ord(c) - ord("a")
+            if curr.children[pos] == None:
+                return False
+            curr = curr.children[pos]
+        return curr.endOfWord
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for c in prefix:
+            pos = ord(c)- ord("a")
+            if curr.children[pos] == None:
+                return False
+            curr = curr.children[pos]
+        return True
+
+# TC: O(n) n = len of string
+# SC: O(t) t = total num of nodes created 
+        
